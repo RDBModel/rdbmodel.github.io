c4c51d23a2a5cc1313cd6ddf9ff8216cd9efccc6
Yauhen Pyl <yauhen.pyl@gmail.com>
2021-12-14 21:45:10 +0100

    Decoder test

--- src/DomainDecoder.elm
+++ src/DomainDecoder.elm
@@ -1,4 +1,5 @@
-module DomainDecoder exposing (domainDecoder, Domain, Actor, Relation, Ring, Delivery, Block)
+module DomainDecoder exposing (domainDecoder, viewsDecoder, View, ViewElement, Domain, Actor, Relation, Ring
+  , Delivery, Block, ViewRelation)
 
 import Yaml.Decode exposing (..)
 import Dict exposing (Dict)
@@ -41,8 +42,27 @@ type alias Relation =
   , description : String
   }
 
+type alias View =
+  { elements: Dict String ViewElement
+  }
+
+type alias ViewElement = 
+  { x : Int
+  , y : Int
+  , relations : Maybe (Dict String (List ViewRelation))
+  }
+
+type alias ViewRelation =
+  { x : Int
+  , y : Int
+  }
+
 domainDecoder : Decoder Domain
 domainDecoder = field "domain" internalDomainDecoder
+
+
+viewsDecoder : Decoder (Dict String View)
+viewsDecoder = field "views" internalViewsDecoder
 
 internalDomainDecoder : Decoder Domain
 internalDomainDecoder =
@@ -88,6 +108,27 @@ relationDecoder : Decoder Relation
 relationDecoder =
   string |> andThen (getRelationFromString >> fromResult)
 
+internalViewsDecoder : Decoder (Dict String View)
+internalViewsDecoder =
+  dict viewDecoder
+
+viewDecoder : Decoder View
+viewDecoder =
+  map View
+    (field "elements" (dict viewElementDecoder))
+
+viewElementDecoder : Decoder ViewElement
+viewElementDecoder =
+  map3 ViewElement
+    (field "x" int)
+    (field "y" int)
+    (maybe (field "relations" (dict (list viewRelationDecoder))))
+
+viewRelationDecoder : Decoder ViewRelation
+viewRelationDecoder =
+  map2 ViewRelation
+    (field "x" int)
+    (field "y" int)
 
 getRelationFromString : String -> Result String Relation
 getRelationFromString value =
