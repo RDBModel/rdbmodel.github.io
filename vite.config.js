394e426721eb0e670530bc9ebeab04fc4efc09b4
Yauhen Pyl <rdb-model@protonmail.com>
2022-10-18 14:26:38 +0200

    no optimize

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { plugin as elmPlugin } from 'vite-plugin-elm'
 import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
 export default defineConfig({
-  plugins: [elmPlugin(), monacoEditorPlugin.default({ languageWorkers: ['editorWorkerService'] })]
+  plugins: [elmPlugin({ optimize: false }), monacoEditorPlugin.default({ languageWorkers: ['editorWorkerService'] })]
 })
