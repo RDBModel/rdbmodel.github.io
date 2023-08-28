0c83769ddc63d60f8abe20307918d7ed57e04bc9
Yauhen Pyl <rdb-model@protonmail.com>
2023-08-28 23:22:38 +0200

    update minor

--- up-version.js
+++ up-version.js
@@ -1,30 +1,38 @@
-import fs from 'fs';
+import fs from 'fs'
+
+const minorUp = process.argv.length === 3
 
 const filePath = "index.js";
-const regexPattern = /const version = '(\d+)\.(\d+)\.(\d+)'/g;
+const regexPattern = /const version = '(\d+)\.(\d+)\.(\d+)'/g
 
 // Read the content of the file
 fs.readFile(filePath, "utf-8", (err, data) => {
   if (err) {
-    console.error("Error reading the file:", err);
-    return;
+    console.error("Error reading the file:", err)
+    return
   }
 
   // Modify the content
   const modifiedContent = data.replace(
     regexPattern,
     (match, major, minor, patch) => {
-      const newPatch = parseInt(patch) + 1;
-      return `const version = '${major}.${minor}.${newPatch}'`;
+      let newPatch = parseInt(patch)
+      let newMinor = parseInt(minor)
+      if (minorUp) {
+        newMinor = parseInt(minor) + 1
+      } else {
+        newPatch = parseInt(patch) + 1
+      }
+      return `const version = '${major}.${newMinor}.${newPatch}'`
     }
-  );
+  )
 
   // Write the modified content back to the file
   fs.writeFile(filePath, modifiedContent, "utf-8", (err) => {
     if (err) {
-      console.error("Error writing to the file:", err);
-      return;
+      console.error("Error writing to the file:", err)
+      return
     }
-    console.log("Line replacement complete.");
-  });
-});
+    console.log("Line replacement complete.")
+  })
+})
