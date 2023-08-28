9cf213163d6fb33ab554d301dd7ee445456610b3
Yauhen Pyl <rdb-model@protonmail.com>
2023-08-28 23:24:11 +0200

    Version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import * as monaco from 'monaco-editor'
 import { Elm } from './src/Main.elm'
 import { setDiagnosticsOptions } from 'monaco-yaml'
 
-const version = '0.2.19'
+const version = '0.2.20'
 
 const app = Elm.Main.init({
   node: document.getElementById('root'),
