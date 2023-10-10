55ae74a805feb7bef1af77afa7c3afc726ce45c2
Yauhen Pyl <rdb-model@protonmail.com>
2023-10-10 20:06:47 +0200

    clean up

--- src/Pages/Editor.elm
+++ src/Pages/Editor.elm
@@ -155,7 +155,7 @@ update msg model =
                 cleaned =
                     String.replace "\u{000D}\n" "\n" val
             in
-            case D.fromString rdbDecoder cleaned |> Debug.log "here" of
+            case D.fromString rdbDecoder cleaned of
                 Ok ( domain, views ) ->
                     let
                         newMonacoValue =
